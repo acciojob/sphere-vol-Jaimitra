5d16a85a65120d05c1edabe64dd35738b7f725ea
Jaimitra <141642600+Jaimitra@users.noreply.github.com>
2023-11-20 22:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
-  
+    let radius = document.querySelector("#radius");
+	let vol = document.querySelector("#volume");
+	vol.value = (4/3)*3.14*(radius.value ** 3);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
